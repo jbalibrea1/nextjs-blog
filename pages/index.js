893f666d2c59ab094321644fc5e62f9450febb89
Jorge Balibrea <jorge.balibrea1@gmail.com>
2022-09-15 02:22:16 +0200

    change name2

--- pages/index.js
+++ pages/index.js
@@ -23,7 +23,7 @@ export default function Home({ allPostsData }) {
       <section className={utilStyles.headingMd}>
         <p>Hello my name is Jorge Balibrea</p>
         <p>
-          (This is a sample website - you’ll be building a site like this on
+          (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
       </section>
